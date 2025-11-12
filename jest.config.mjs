e31eefe0a0746871668d95d5d861e4a6a83b4323
ashiz123 <37142670+ashiz123@users.noreply.github.com>
2025-11-12 00:52:02 +0000

    App initializing

---
+++ jest.config.mjs
@@ -0,0 +1,18 @@
+export default {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.ts$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+    roots: ['./tests'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+import {add} from "./opertions/add.js";
+
+
+console.log(add(5, 4));
+
